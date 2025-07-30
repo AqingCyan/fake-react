d985f6f841f53caf2d18de3e155924c44ae48aa0
AqingCyan <aqingcyan@gmail.com>
2025-07-30 18:01:24 +0800

    feat: 实现 completeWork 函数的逻辑，支持 HostComponent 和 HostText 的 DOM 创建与更新，同时添加临时的宿主环境方法以便于调试

--- packages/react-reconciler/src/completeWork.ts
+++ packages/react-reconciler/src/completeWork.ts
@@ -1,7 +1,76 @@
-// 递归中的“归”阶段
 import { FiberNode } from './fiber';
+import { HostComponent, HostRoot, HostText } from './workTags';
+import {
+	appendInitialChild,
+	createInstance,
+	createTextInstance
+} from 'hostConfig';
 
-export const completeWork = (fiber: FiberNode): FiberNode | null => {
-	console.log(fiber);
+// 递归中的“归”阶段
+// 对于 Host 类型的 fiberNode，需要构建离屏的 DOM 树
+// TODO: 标记 update flag
+export const completeWork = (wip: FiberNode): FiberNode | null => {
+	const newProps = wip.pendingProps;
+	const current = wip.alternate;
+
+	switch (wip.tag) {
+		case HostComponent:
+			if (current !== null && wip.stateNode) {
+				// update 阶段
+			} else {
+				// 首屏渲染的阶段
+				// 1. 构建 DOM；
+				const instance = createInstance(wip.type, newProps);
+				// 2. 将 DOM 插入到 DOM 树中
+				appendAllChildren(instance, wip);
+				wip.stateNode = instance;
+			}
+			return null;
+		// 1. 构建 DOM；2. 将 DOM 插入到 DOM 树中
+		case HostText:
+			if (current !== null && wip.stateNode) {
+				// update 阶段
+			} else {
+				// 首屏渲染的阶段
+				// 1. 构建 DOM；2. 将 DOM 插入到 DOM 树中
+				wip.stateNode = createTextInstance(newProps.content);
+			}
+			return null;
+		case HostRoot:
+			return null;
+		default:
+			if (__DEV__) {
+				console.warn('未处理的 completeWork 情况', wip.tag);
+			}
+	}
+
 	return null;
 };
+
+function appendAllChildren(parent: FiberNode, wip: FiberNode) {
+	let node = wip.child;
+
+	while (node !== null) {
+		if (node.tag === HostComponent || node.tag === HostText) {
+			appendInitialChild(parent, node.stateNode);
+		} else if (node.child !== null) {
+			node.child.return = node;
+			node = node.child;
+			continue;
+		}
+
+		if (node === wip) {
+			return;
+		}
+
+		// 往下找没找着，兄弟节点也没有，就该往上归了
+		while (node.sibling === null) {
+			if (node.return === null || node.return === wip) {
+				return;
+			}
+			node = node.return;
+		}
+		node.sibling.return = node.return;
+		node = node.sibling;
+	}
+}
