0cc17683cbd2344a54d7b7d22e1918d4eaee249f
AqingCyan <aqingcyan@gmail.com>
2025-07-30 14:49:44 +0800

    fix: 修复错误泛型名称

--- packages/react-reconciler/src/updateQueue.ts
+++ packages/react-reconciler/src/updateQueue.ts
@@ -24,12 +24,12 @@ export const createUpdate = <State>(action: Action<State>): Update<State> => {
  * createUpdateQueue 更新队列的实例化方法
  * 初始化一个空的更新队列，每个 Fiber 节点都会有自己的更新队列
  */
-export const createUpdateQueue = <Action>() => {
+export const createUpdateQueue = <State>() => {
 	return {
 		shared: {
 			pending: null
 		}
-	} as UpdateQueue<Action>;
+	} as UpdateQueue<State>;
 };
 
 /**
@@ -39,9 +39,9 @@ export const createUpdateQueue = <Action>() => {
  * @param updateQueue
  * @param update
  */
-export const enqueueUpdate = <Action>(
-	updateQueue: UpdateQueue<Action>,
-	update: Update<Action>
+export const enqueueUpdate = <State>(
+	updateQueue: UpdateQueue<State>,
+	update: Update<State>
 ) => {
 	updateQueue.shared.pending = update;
 };
